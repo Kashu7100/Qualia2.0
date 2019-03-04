d31995bd07198696221c604e243567615cb92933
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-03-04 14:44:38 -0600

    Update array.py

--- qualia2/functions/array.py
+++ qualia2/functions/array.py
@@ -25,3 +25,19 @@ class Transpose(Function):
         return np.transpose(dx, [self.kwargs['axes'].index(i) for i in range(len(self.kwargs['axes']))]) 
 
 transpose = Transpose(None)
+
+class ListConcat(Function):
+    '''
+    Concatenate list of Tensors 
+    '''
+    @staticmethod
+    def forward(list):
+        result = Tensor(np.concatenate([np.expand_dims(arr.data, axis=0) for arr in list], axis=0))
+        result.set_creator(ListConcat.prepare(result.shape, *list))
+        return result
+    
+    def calc_grad(self, dx):
+        result = np.split(dx, len(self.var))
+        return [np.squeeze(r, axis=0) for r in result]
+
+listconcat = ListConcat(None)
