b278fad9b377bfd459234597bf8e5a2491b906fd
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-03-18 04:33:42 -0500

    Update util.py

--- qualia2/util.py
+++ qualia2/util.py
@@ -5,6 +5,7 @@ from .core import *
 from .autograd import Tensor
 from functools import reduce
 import sys
+import random
 import time
 from datetime import timedelta
 import matplotlib.pyplot as plt
@@ -121,3 +122,66 @@ def tester(model, dataloader, minibatch, filename):
         acc += sum(out == ans)/label.shape[0]
         progressbar(i, len(dataloader))
     print('\n[*] test acc: {:.2f}%'.format(float(acc/len(dataloader)*100)))
+
+class ReplayMemory(object):
+    '''Replay Memory\n
+    Memory class for experience replay. 
+    Args:
+        capacity (int): capacity of the memory
+    '''
+    def __init__(self, capacity=10000):
+        self.capacity = capacity
+        self.transitions = None
+        self.idx = 0
+        self.batch = 1
+        self.is_full = False
+
+    def __len__(self):
+        return self.capacity if self.is_full else self.idx
+    
+    def sample(self):
+        if self.transitions is None:
+            raise Exception('[*] Cannot sample from empty memory.')
+        if not self.is_full:
+            assert self.idx > self.batch
+        i = random.sample(list(range(self.capacity)), self.batch) if self.is_full else random.sample(list(range(self.idx)), self.batch)
+        state, action, next, reward = (self.transitions[i,:self.states],
+                                       self.transitions[i,self.states:self.states+self.actions],
+                                       self.transitions[i,self.states+self.actions:2*self.states+self.actions],
+                                       self.transitions[i,2*self.states+self.actions:])
+        return Tensor(state.reshape(self.batch,-1), requires_grad=False), action.reshape(self.batch,-1).astype(np.int32), Tensor(next.reshape(self.batch,-1), requires_grad=False), reward.reshape(self.batch,-1)
+
+    def shuffle(self):
+        if not self.is_full:
+            raise Exception('[*] shuffle of non-full memory is prohibited.')
+        i = np.random.permutation(self.capacity)
+        self.transitions = self.transitions[i]
+
+    @staticmethod
+    def get_len(obj):
+        if type(obj) is int or type(obj) is float:
+            return 1
+        else:
+            return len(obj)
+
+    def append(self, state, action, nextstate, reward):
+        '''
+        Args:
+            state (ndarray|int|float): current state
+            action (ndarray|int|float): action decided
+            nextstate (ndarray|int|float): next state
+            reward (ndarray|int|float): reward of the action took
+        '''
+        if self.transitions is None:
+            self.states = ReplayMemory.get_len(state)
+            self.actions = ReplayMemory.get_len(action)
+            self.rewards = ReplayMemory.get_len(reward)
+            self.transitions = np.zeros((self.capacity, 2*self.states+self.actions+self.rewards))
+        if len(self) < self.capacity // self.batch:
+            if (self.idx+1)//self.capacity > 0:
+                self.is_full = True
+        self.transitions[self.idx,:self.states] = state
+        self.transitions[self.idx,self.states:self.states+self.actions] = action
+        self.transitions[self.idx,self.states+self.actions:2*self.states+self.actions] = nextstate
+        self.transitions[self.idx,2*self.states+self.actions:] = reward
+        self.idx = (self.idx+1)%self.capacity
