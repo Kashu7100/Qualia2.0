e8cc052d5441e0fd3dc7d941ade60b919adca4f7
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-03-20 17:37:40 -0500

    Add files via upload

---
+++ qualia2/functions/dropout.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*- 
+from ..core import *
+from ..autograd import *
+
+class Dropout(Function):
+    @staticmethod
+    def forward(x, p=0.5, training=True):
+        '''
+        During training, randomly zeroes some of the elements of the input tensor with probability p using samples from a Bernoulli distribution. 
+        Each channel will be zeroed out independently on every forward call.
+        Args:
+            x (Tensor): Input tensor with any shepe
+            p (float): probability that randomly zeroes some of the elements of the input tensor
+            training (bool): True if the model is in training
+        '''
+        if training:
+            np.random.seed()
+            mask = (np.random.binomial(1,p,x.shape) == 1)
+            tmp = x.data.copy()
+            tmp[mask] = 0
+            result = Tensor(tmp)
+            result.set_creator(Dropout.prepare(result.shape, x, mask=mask))
+            return result
+        else:
+            return x        
+    
+    def calc_grad(self, dx):
+        tmp = dx.copy()
+        tmp[self.kwargs['mask']] = 0
+        return tmp
+
+dropout = Dropout(None)
