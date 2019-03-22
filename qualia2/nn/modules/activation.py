e50f79cbc423300b382d699af9db60f7241e2aa5
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-03-22 04:56:12 -0500

    Add files via upload

---
+++ qualia2/nn/modules/activation.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*- 
+from .module import Module
+from ...functions import *
+
+class ReLU(Module):
+    def forward(self, x):
+        if self.input_shape is None:
+            self.input_shape = x.shape
+        if self.output_shape is None:
+            self.output_shape = x.shape
+        return relu(x)
+
+class LeakyReLU(Module):
+    def forward(self, x):
+        if self.input_shape is None:
+            self.input_shape = x.shape
+        if self.output_shape is None:
+            self.output_shape = x.shape
+        return leakyrelu(x)
+
+class ELU(Module):
+    def __init__(self, alpha=1.0):
+        super().__init__()
+        self.alpha = alpha
+
+    def forward(self, x):
+        if self.input_shape is None:
+            self.input_shape = x.shape
+        if self.output_shape is None:
+            self.output_shape = x.shape
+        return elu(x, self.alpha)
+
+class Sigmoid(Module):
+    def forward(self, x):
+        if self.input_shape is None:
+            self.input_shape = x.shape
+        if self.output_shape is None:
+            self.output_shape = x.shape
+        return sigmoid(x)
+
+class Tanh(Module):
+    def forward(self, x):
+        if self.input_shape is None:
+            self.input_shape = x.shape
+        if self.output_shape is None:
+            self.output_shape = x.shape
+        return tanh(x)
+
+class SoftPuls(Module):
+    def forward(self, x):
+        if self.input_shape is None:
+            self.input_shape = x.shape
+        if self.output_shape is None:
+            self.output_shape = x.shape
+        return softplus(x)
+
+class SoftSign(Module):
+    def forward(self, x):
+        if self.input_shape is None:
+            self.input_shape = x.shape
+        if self.output_shape is None:
+            self.output_shape = x.shape
+        return softsign(x)
+
+class SoftMax(Module):
+    def forward(self, x):
+        if self.input_shape is None:
+            self.input_shape = x.shape
+        if self.output_shape is None:
+            self.output_shape = x.shape
+        return softmax(x)
