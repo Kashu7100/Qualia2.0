558d2bd42504428f0ae54623222c61fd4710f7d8
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-03-26 13:43:29 -0500

    Update core.py

--- qualia2/core.py
+++ qualia2/core.py
@@ -1,25 +1,47 @@
 # -*- coding: utf-8 -*- 
 import os
+import time
 from .config import *
+import subprocess
+from logging import getLogger, Formatter, FileHandler, StreamHandler
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+if not os.path.exists(path + '/logs/'):
+    os.makedirs(path + '/logs/') 
+
+logger = getLogger('QualiaLogger')
+logger.setLevel(level)
+filehandler = FileHandler(filename=path+'/logs/{}.log'.format(time.strftime('%Y%m%d-%H%M%S')), mode='a')
+streamhandler = StreamHandler()
+filehandler.setLevel(level)
+streamhandler.setLevel(level)
+formatter = Formatter(fmt, datefmt)
+filehandler.setFormatter(formatter)
+logger.addHandler(filehandler)
+logger.addHandler(streamhandler)
 
 if gpu:
     import cupy as np
     if not np.cuda.is_available():
+        logger.error('[*] CUDA device is not available.')
         raise Exception('[*] CUDA device is not available.')
     np.cuda.set_allocator(np.cuda.MemoryPool().malloc)
     np.add.at = np.scatter_add
 
-    print('[*] GPU acceleration enabled.')
-    print('-'*71)
-    os.system('nvcc --version')
-    print('-'*71)
-    
+    logger.info('[*] GPU acceleration enabled.')
+    logger.info('-'*71)
+    nvcc = subprocess.check_output('nvcc --version', shell=True)
+    for line in nvcc.split(b'\n'):
+        logger.info(line.decode("utf-8"))
+    logger.info('-'*71)
+
     def to_cpu(obj):
         return np.asnumpy(obj)
-    
+
     def to_gpu(obj):
         return np.asarray(obj)
-
+        
 else:
     import numpy as np 
 
@@ -28,5 +50,3 @@ else:
 
     def to_gpu(obj):
         raise Exception('[*] GPU acceleration is disabled.')
-
-dtype = np.float32
