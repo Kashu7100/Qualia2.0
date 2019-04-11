af3509934387ae5a99bf37d3ad05733174ac7ae7
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-04-11 04:25:26 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(name='qualia2',
+      version='0.0.1',
+      description='Qualia2.0 is a deep learning framework deeply integrated with automatic differentiation and dynamic graphing with CUDA acceleration. ',
+      author='Kashu',
+      author_email='echo_six0566@yahoo.co.jp',
+      url='https://github.com/Kashu7100/Qualia2.0',
+      packages=find_packages(),)
