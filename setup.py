38ec418dece8104463cbf053764ce54b281ff8fc
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-04-11 15:20:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,9 @@ setup(name='qualia2',
       author='Kashu',
       author_email='echo_six0566@yahoo.co.jp',
       url='https://github.com/Kashu7100/Qualia2.0',
-      packages=find_packages(),)
+      packages=find_packages(),
+      install_requires=[
+            'numpy',
+            'matplotlib',
+            'h5py'
+      ],)
