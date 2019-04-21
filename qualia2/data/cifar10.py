39f9d31e55db3679e8a2945d8e59fe4f8b9958c0
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-04-20 19:05:36 -0500

    Update cifar10.py

--- qualia2/data/cifar10.py
+++ qualia2/data/cifar10.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*- 
+from .. import to_cpu
+from ..core import *
+from ..config import gpu
+from ..autograd import Tensor
+from .dataloader import DataLoader
+import matplotlib.pyplot as plt
+import os
+import gzip
 
+class CIFAR10(DataLoader):
+  pass
