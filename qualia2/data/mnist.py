a246a5194661a3a8053ba993c67f3fb6da320ebf
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-05-16 17:07:09 -0500

    Update mnist.py

--- qualia2/data/mnist.py
+++ qualia2/data/mnist.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*- 
 from .. import to_cpu
 from ..core import *
-from ..config import gpu
 from ..autograd import Tensor
 from .dataloader import DataLoader
 import matplotlib.pyplot as plt
