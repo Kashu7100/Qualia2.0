92dad0cdd3a60b492c93989f0c051a61ff2934d6
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-06-06 15:34:28 -0500

    Update loss.py

--- qualia2/functions/loss.py
+++ qualia2/functions/loss.py
@@ -37,6 +37,41 @@ class MSELoss(Function):
 
 mse_loss = MSELoss(None)
 
+class CMSELoss(Function):
+    '''MSE Loss for complex neural networks\n
+    Args:
+        input (Tensor): output of the network
+        target (Tensor): label of the dataset
+        reduce (bool): the losses are averaged or summed over observations for each minibatch depending on size_average. 
+        size_average (bool): the losses are averaged over each loss element in the batch.
+
+    Model:
+        l_n = sum((y_n - x_n)^2/2)
+    
+    Shape:
+        - Input: [N, C] 
+        - Target: [N, C] 
+        - Output: [1] by default 
+                  [N] if not reduce
+    '''
+    @staticmethod
+    def forward(input, target, reduce=True, size_average=True):
+        tmp = np.sum(np.divide(np.power(np.abs(np.subtract(input.data, target.data)), 2), 2), axis=1)
+        if reduce:
+            if size_average:
+                result = Tensor(np.mean(tmp,axis=0))
+            else:
+                result = Tensor(np.sum(tmp,axis=0))
+        else:
+            result = Tensor(tmp)
+        result.set_creator(CMSELoss.prepare(result.shape, input, target))
+        return result
+
+    def calc_grad(self, dx):
+        return np.subtract(self.var[0].data, self.var[1].data), np.subtract(self.var[1].data, self.var[0].data)
+
+cmse_loss = CMSELoss(None)
+
 class HuberLoss(Function):
     '''
     Args:
