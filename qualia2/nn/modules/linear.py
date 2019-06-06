68b073c5cf9e6430f0a0ffad209261ee4f07f1bb
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-06-06 03:53:27 -0500

    Update linear.py

--- qualia2/nn/modules/linear.py
+++ qualia2/nn/modules/linear.py
@@ -41,3 +41,40 @@ class Linear(Module):
         if self.output_shape is None:
             self.output_shape = result.shape
         return result
+    
+class CLinear(Module):
+    '''Applies a complex linear transformation to the incoming data\n 
+    Model: 
+        y = x*w.T + b 
+     
+    Args: 
+        in_features (int): size of each input sample 
+        out_features (int): size of each output sample 
+        bias (bool): whether to use bias. Default: True 
+     
+    Shape: 
+        - Input: [N, *, in_features] where '∗' means any number of additional dimensions. 
+        - Output: [N, *, out_features] where '∗' means any number of additional dimensions. 
+    ''' 
+    def __init__(self, in_features, out_features, bias=True): 
+        super().__init__() 
+        self.in_features = in_features
+        self.out_features = out_features
+        self.num_params += in_features*out_features
+        self.weight = Tensor(np.random.normal(0, math.sqrt(1/in_features),(in_features, out_features))+1j*np.random.normal(0, math.sqrt(1/in_features),(in_features, out_features))) 
+        if bias: 
+            self.bias = Tensor(np.zeros(out_features)+1j*np.zeros(out_features)) 
+            self.num_params += out_features
+        else: 
+            self.bias = None
+            
+    def __repr__(self):
+        return '{}({}, {}, bias={}) at 0x{:0{}X}'.format(self.__class__.__name__, self.in_features, self.out_features, str(self.bias is not None), id(self), 16)
+    
+    def forward(self, x): 
+        result = linear(x, self.weight, self.bias)
+        if self.input_shape is None:
+            self.input_shape = x.shape
+        if self.output_shape is None:
+            self.output_shape = result.shape
+        return result
