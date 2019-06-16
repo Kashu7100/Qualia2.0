e5868c43708c3dbe1642bf5585fc7d330d82ba06
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-06-16 03:38:20 -0500

    Update core.py

--- qualia2/core.py
+++ qualia2/core.py
@@ -27,25 +27,29 @@ logger.addHandler(filehandler)
 logger.addHandler(streamhandler)
 
 if gpu:
-    import cupy as np
-    if not np.cuda.is_available():
-        logger.error('[*] CUDA device is not available.')
-        raise Exception
-    np.cuda.set_allocator(np.cuda.MemoryPool().malloc)
+    try:
+        import cupy as np
+        if np.cuda.is_available():
+            np.cuda.set_allocator(np.cuda.MemoryPool().malloc)
+            logger.info('[*] GPU acceleration enabled.')
+            logger.info('-'*71)
+            nvcc = subprocess.check_output('nvcc --version', shell=True)
+            for line in nvcc.split(b'\n'):
+                logger.info(line.decode("utf-8"))
+            logger.info('-'*71)
+            
+            def to_cpu(obj):
+                return np.asnumpy(obj)
 
-    logger.info('[*] GPU acceleration enabled.')
-    logger.info('-'*71)
-    nvcc = subprocess.check_output('nvcc --version', shell=True)
-    for line in nvcc.split(b'\n'):
-        logger.info(line.decode("utf-8"))
-    logger.info('-'*71)
-
-    def to_cpu(obj):
-        return np.asnumpy(obj)
-
-    def to_gpu(obj):
-        return np.asarray(obj)
-
+            def to_gpu(obj):
+                return np.asarray(obj)
+            
+        else:
+            logger.error('[*] CUDA device is not available.')
+            import numpy as np
+    except:
+        import numpy as np
+        
 else:
     import numpy as np 
 
