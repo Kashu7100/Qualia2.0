1aa8db59c283f4e5ab0af1d2ef31c95860c53a47
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-06-16 04:21:42 -0500

    Update __init__.py

--- qualia2/__init__.py
+++ qualia2/__init__.py
@@ -11,35 +11,35 @@ from .environment import *
 pi = np.pi
 e = np.e
 
-def array(obj):
+def array(obj, dtype='float64'):
     return Tensor(np.array(obj, dtype=dtype))
 
-def arange(*args):
+def arange(*args, dtype='float64'):
     return Tensor(np.arange(*args, dtype=dtype))
 
-def zeros(shape):
+def zeros(shape, dtype='float64'):
     return Tensor(np.zeros(shape, dtype=dtype))
 
-def zeros_like(obj):
+def zeros_like(obj, dtype='float64'):
     return Tensor(np.zeros(obj.shape, dtype=dtype))
 
-def ones(shape):
+def ones(shape, dtype='float64'):
     return Tensor(np.ones(shape, dtype=dtype))
 
-def ones_like(obj):
+def ones_like(obj, dtype='float64'):
     return Tensor(np.ones(obj.shape, dtype=dtype))
 
-def rand(*args):
+def rand(*args, dtype='float64'):
     return Tensor(np.random.rand(*args).astype(dtype))
 
-def rand_like(obj):
+def rand_like(obj, dtype='float64'):
     return Tensor(np.random.rand(*obj.shape).astype(dtype))
 
-def randn(*args):
+def randn(*args, dtype='float64'):
     return Tensor(np.random.randn(*args).astype(dtype))
 
-def randn_like(obj):
+def randn_like(obj, dtype='float64'):
     return Tensor(np.random.randn(*obj.shape).astype(dtype))
 
-def uniform(*args):
+def uniform(*args, dtype='float64'):
     return Tensor(np.random.uniform(*args).astype(dtype))
