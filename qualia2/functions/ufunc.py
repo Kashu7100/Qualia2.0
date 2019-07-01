92e5bee157273678ecc447e2469ea2b5e54f28f1
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-07-01 01:43:04 -0500

    added Max and Min Function

--- qualia2/functions/ufunc.py
+++ qualia2/functions/ufunc.py
@@ -105,3 +105,67 @@ class Sum(Function):
         return np.tile(dx,[shape[i] if i in self.kwargs['axis'] else 1 for i in range(len(shape))])
 
 sum = Sum(None)
+
+class Max(Function):
+    @staticmethod
+    def forward(a, axis=1):
+        result = Tensor(np.amax(a.data, axis=axis, keepdims=True))
+        result.set_creator(Max.prepare(result.shape, a, axis=axis))
+        return result
+    
+    def calc_grad(self, dx):
+        result = np.zeros_like(self.var[0].data)
+        argmax = np.argmax(self.var[0].data, axis=self.kwargs['axis']).reshape(*[s if i != self.kwargs['axis'] else 1 for i, s in enumerate(result.shape)])
+        def make_slice(arr, dim, i):
+            slc = [slice(None)] * arr.ndim
+            slc[dim] = i
+            return slc
+        idx_xsection_shape = argmax.shape[:self.kwargs['axis']] + argmax.shape[self.kwargs['axis']+1:]
+
+        idx = [[np.indices(idx_xsection_shape).reshape(argmax.ndim-1, -1), argmax[make_slice(argmax, self.kwargs['axis'], i)].reshape(1, -1)] for i in range(argmax.shape[self.kwargs['axis']])]
+        idx = list(np.concatenate(tuple(idx[0]), axis=0))
+        idx.insert(self.kwargs['axis'], idx.pop())
+
+        if not np.isscalar(dx):
+            src_xsection_shape = dx.shape[:self.kwargs['axis']] + dx.shape[self.kwargs['axis'] + 1:]
+            src_idx = list(idx)
+            src_idx.pop(self.kwargs['axis'])
+            src_idx.insert(self.kwargs['axis'], np.repeat(np.arange(argmax.shape[self.kwargs['axis']]), reduce(lambda a, b: a*b, idx_xsection_shape)))
+            result[idx] = dx[src_idx]
+        else:
+            result[idx] = dx
+        return result
+
+max = Max(None)
+
+class Min(Function):
+    @staticmethod
+    def forward(a, axis=1):
+        result = Tensor(np.amin(a.data, axis=axis, keepdims=True))
+        result.set_creator(Min.prepare(result.shape, a, axis=axis))
+        return result
+    
+    def calc_grad(self, dx):
+        result = np.zeros_like(self.var[0].data)
+        argmin = np.argmin(self.var[0].data, axis=self.kwargs['axis']).reshape(*[s if i != self.kwargs['axis'] else 1 for i, s in enumerate(result.shape)])
+        def make_slice(arr, dim, i):
+            slc = [slice(None)] * arr.ndim
+            slc[dim] = i
+            return slc
+        idx_xsection_shape = argmin.shape[:self.kwargs['axis']] + argmin.shape[self.kwargs['axis']+1:]
+
+        idx = [[np.indices(idx_xsection_shape).reshape(argmin.ndim-1, -1), argmin[make_slice(argmin, self.kwargs['axis'], i)].reshape(1, -1)] for i in range(argmin.shape[self.kwargs['axis']])]
+        idx = list(np.concatenate(tuple(idx[0]), axis=0))
+        idx.insert(self.kwargs['axis'], idx.pop())
+
+        if not np.isscalar(dx):
+            src_xsection_shape = dx.shape[:self.kwargs['axis']] + dx.shape[self.kwargs['axis'] + 1:]
+            src_idx = list(idx)
+            src_idx.pop(self.kwargs['axis'])
+            src_idx.insert(self.kwargs['axis'], np.repeat(np.arange(argmin.shape[self.kwargs['axis']]), reduce(lambda a, b: a*b, idx_xsection_shape)))
+            result[idx] = dx[src_idx]
+        else:
+            result[idx] = dx
+        return result
+
+min = Min(None)
