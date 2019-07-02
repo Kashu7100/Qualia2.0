8715f0d3d1ab4c8dc64358c4595898e99301a87a
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-07-02 18:25:43 -0500

    Create __init__.py

---
+++ qualia2/rl/envs/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .inverted_pendulum import  *
+from .mountain_car import *
+from .atari import *
+from .bipedal_walker import *
+from .lunar_lander import *
+from .reacher import *
