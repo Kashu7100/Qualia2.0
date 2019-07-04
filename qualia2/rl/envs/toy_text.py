c7c364d3eb6108d16be81e70e1176d55c79f01db
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-07-04 03:20:39 -0500

    Add files via upload

---
+++ qualia2/rl/envs/toy_text.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*- 
+from ..core import Env, Tensor
+
+class FrozenLake(Env):
+    ''' FrozenLake\n
+    The agent controls the movement of a character in a grid world. 
+    Some tiles of the grid are walkable, and others lead to the agent falling into the water. 
+    Additionally, the movement direction of the agent is uncertain and only partially depends on the chosen direction. 
+    The agent is rewarded for finding a walkable path to a goal tile.
+
+        SFFF       (S: starting point, safe)
+        FHFH       (F: frozen surface, safe)
+        FFFH       (H: hole, fall to your doom)
+        HFFG       (G: goal, where the frisbee is located)
+    
+    Reference:
+        https://gym.openai.com/envs/FrozenLake-v0/
+    '''
+    def __init__(self):
+        super().__init__('FrozenLake-v0')
+    
+class FrozenLake8x8(Env):
+    ''' FrozenLake8x8\n
+    The agent controls the movement of a character in a grid world. 
+    Some tiles of the grid are walkable, and others lead to the agent falling into the water. 
+    Additionally, the movement direction of the agent is uncertain and only partially depends on the chosen direction. 
+    The agent is rewarded for finding a walkable path to a goal tile.
+
+        SFFF       (S: starting point, safe)
+        FHFH       (F: frozen surface, safe)
+        FFFH       (H: hole, fall to your doom)
+        HFFG       (G: goal, where the frisbee is located)
+    
+    Reference:
+        https://gym.openai.com/envs/FrozenLake8x8-v0/
+    '''
+    def __init__(self):
+        super().__init__('FrozenLake8x8-v0')
