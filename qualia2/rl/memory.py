38e6b88db70eb7ad64729b9fd653a26e6ed4cc5f
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-07-04 09:23:12 -0500

    Update memory.py

--- qualia2/rl/memory.py
+++ qualia2/rl/memory.py
@@ -15,9 +15,15 @@ class ReplayMemory(deque):
         else:
             return super().__getitem__(idx)
     
-    def sample(self, batch_size, aslist=False):
-        if aslist:
-            return random.sample(self, batch_size)
-        tmp = [*zip(*random.sample(self, batch_size))]
-        return Experience(*[listconcat(tmp[i]).detach() for i in range(3)],np.concatenate(tuple([np.asarray(i).reshape(1,-1) for i in tmp[3]]),axis=0),np.array(tmp[4]))
-        
+    def sample(self, batch_size, steps=1):
+        assert steps > 0
+        if steps > 1:
+            idx = random.sample(range(len(self)-steps),batch_size)
+            result = []
+            for step in range(steps):
+                tmp = [*zip(*[self[i+step] for i in idx])]
+                result.append(Experience(*[listconcat(tmp[i]).detach() for i in range(3)],np.concatenate(tuple([np.asarray(i).reshape(1,-1) for i in tmp[3]]),axis=0),np.array(tmp[4])))
+            return result
+        else:
+            tmp = [*zip(*random.sample(self, batch_size))]
+            return Experience(*[listconcat(tmp[i]).detach() for i in range(3)],np.concatenate(tuple([np.asarray(i).reshape(1,-1) for i in tmp[3]]),axis=0),np.array(tmp[4])) 
