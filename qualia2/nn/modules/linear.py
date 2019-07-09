8f393e931e09dc083db6f075f8b17372a741559b
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-07-09 03:46:10 -0500

    added NoisyLinear
    NoisyNet, a deep reinforcement learning agent with parametric noise added to its weights

--- qualia2/nn/modules/linear.py
+++ qualia2/nn/modules/linear.py
@@ -78,3 +78,65 @@ class CLinear(Module):
         if self.output_shape is None:
             self.output_shape = result.shape
         return result
+
+class NoisyLinear(Module):
+    '''Applies a linear transformation with parametric noise added to its weights\n
+    Model:
+        y = (w+sig_w*eps_w)*x+(b+sig_b*eps_b)
+
+    Args:
+        in_features (int): size of each input sample 
+        out_features (int): size of each output sample 
+        std_init (float): std for initializing weights
+        factorised_noise (bool): 
+    
+    Shape: 
+        - Input: [N, *, in_features] where '∗' means any number of additional dimensions. 
+        - Output: [N, *, out_features] where '∗' means any number of additional dimensions. 
+    
+    Reference:
+        https://arxiv.org/abs/1706.10295
+    '''
+    def __init__(self, in_features, out_features, std_init=0.4, factorised_noise=True):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.std_init = std_init
+        self.factorised_noise = factorised_noise
+        mu_range = 1.0 / math.sqrt(self.in_features)
+        self.weight_mu = Tensor(np.random.uniform(-mu_range, mu_range, (out_features, in_features)))
+        self.weight_sigma = Tensor(np.empty(out_features, in_features))
+        self.weight_sigma.fill(self.std_init / math.sqrt(self.in_features))
+        self.weight_epsilon = Tensor(np.empty(out_features, in_features), requires_grad=False)
+        self.bias_mu = Tensor(np.random.uniform(-mu_range, mu_range, (out_features)))
+        self.bias_sigma = Tensor(np.empty(out_features))
+        self.bias_sigma.fill(self.std_init / math.sqrt(self.out_features))
+        self.bias_epsilon = Tensor(np.empty(out_features), requires_grad=False)
+        self.sample_noise()
+
+    def reset_params(self):
+        mu_range = 1.0 / math.sqrt(self.in_features)
+        self.weight_mu.data = np.random.uniform(-mu_range, mu_range, (out_features, in_features))
+        self.weight_sigma.fill(self.std_init / math.sqrt(self.in_features))
+        self.bias_mu.data = np.random.uniform(-mu_range, mu_range, (out_features))
+        self.bias_sigma.fill(self.std_init / math.sqrt(self.out_features))
+
+    def _scale_noise(self, size):
+        x = np.random.randn(size)
+        return np.sign(x)*(np.sqrt(np.abs(x)))
+
+    def sample_noise(self):
+        if self.factorised_noise:
+            epsilon_in = self._scale_noise(self.in_features)
+            epsilon_out = self._scale_noise(self.out_features)
+            self.weight_epsilon.data = np.outer(epsilon_out, epsilon_in)
+            self.bias_epsilon.data = epsilon_out
+        else:
+            self.weight_epsilon.data = np.random.randn(self.out_features, self.in_features)
+            self.bias_epsilon.data = np.random.randn(self.out_features)
+
+    def forward(self, inp):
+        if self.training:
+            return tensordot(inp, self.weight_mu + self.weight_sigma * self.weight_epsilon) + self.bias_mu + self.bias_sigma * self.bias_epsilon
+        else:
+            return tensordot(inp, self.weight_mu) + self.bias_mu
