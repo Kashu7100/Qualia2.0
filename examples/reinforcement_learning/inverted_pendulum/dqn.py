6d4557aa33afa90b7e033c7e16c7810a5534fe32
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-07-12 01:34:31 -0500

    Update dqn.py

--- examples/reinforcement_learning/inverted_pendulum/dqn.py
+++ examples/reinforcement_learning/inverted_pendulum/dqn.py
@@ -21,30 +21,32 @@ class Model(Module):
         x = tanh(self.linear2(x))
         x = tanh(self.linear3(x))
         return x
-
+        
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='DQN example with Qualia2.0')
+    parser = argparse.ArgumentParser(description='Dueling Net example with Qualia2.0')
     parser.add_argument('mode', metavar='str', type=str, choices=['train', 'test'], help='select mode to run the model : train or test.')
-    parser.add_argument('-i', '--itr', type=int, default=100, help=' Number of iterations for the training. Default: 100')
-    parser.add_argument('-c', '--capacity', type=int, default=1024, help='Capacity of the replay memory. Default: 1024')
-    parser.add_argument('-b', '--batch', type=int, default=100, help='Batch size to train the model. Default: 100')
-    parser.add_argument('-s', '--save', type=bool, default=False, help='Save mp4 video of the result. Default: False')
-    parser.add_argument('-p', '--plot', type=bool, default=False, help='Plot rewards over the training. Default: False')
+    parser.add_argument('-i', '--itr', type=int, default=200, help=' Number of iterations for the training. Default: 500')
+    parser.add_argument('-c', '--capacity', type=int, default=10000, help='Capacity of the replay memory. Default: 10000')
+    parser.add_argument('-b', '--batch', type=int, default=80, help='Batch size to train the model. Default: 32')
+    parser.add_argument('-s', '--save', type=bool, default=False, help='Save mp4 video of the result. Default: True')
+    parser.add_argument('-p', '--plot', type=bool, default=True, help='Plot rewards over the training. Default: True')
+    
     args = parser.parse_args()
     
     env = CartPole()
+    agent = DQN.init(env, Model())
+    agent.set_optim(Adadelta)
     
     if args.mode == 'train':
         trainer = DQNTrainer(ReplayMemory,args.batch,args.capacity)
-        agent = trainer.train(env, Model, Adadelta, episodes=args.itr, filename=path+'/dqn_example')
+        agent = trainer.train(env, agent, episodes=args.itr, filename=path+'/dueling_net_example')
         if args.plot:
             trainer.plot()
-            
     if args.mode == 'test':
-        agent = DQN.reload(env, Model)
-        agent.load(path+'/dqn_example')
+        agent = DDQN.init(env, Model())
+        agent.load(path+'/dueling_net_example')
         
     if args.save:
-        agent.play(env, filename=path+'/dqn_cartpole')
+        agent.play(env, filename=path+'/dueling_net_cartpole_example')
     else:
         agent.play(env)
