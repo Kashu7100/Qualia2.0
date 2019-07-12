be4718ed60004334f1873281bb0cd3bc2e57b2fe
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-07-12 03:10:44 -0500

    Update util.py

--- qualia2/rl/util.py
+++ qualia2/rl/util.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
+from ..core import *
 from .memory import Experience, PrioritizedMemory
 import matplotlib.pyplot as plt
 from logging import getLogger
 logger = getLogger('QualiaLogger').getChild('rl')
 
 class Trainer(object):
-    ''' Trainer \n
+    ''' Trainer for RL agent\n
     Args:
         memory (deque): replay memory object
         capacity (int): capacity of the memory
         batch (int): batch size for training
         gamma (int): gamma value
     '''
-    def __init__(self, memory, batch=50, capacity=1024, gamma=0.9):
+    def __init__(self, memory, batch, capacity, gamma):
         self.batch = batch
         self.capacity = capacity
         self.gamma = gamma
@@ -24,10 +25,45 @@ class Trainer(object):
         print('{}'.format(self.__class__.__name__))
 
     def train(self, env, model, episodes=200, render=False, filename=None):
-        raise NotImplementedError
-
+        raise NotImplementedError  
+    
+    def before_train(self, env, agent):
+        self.env_name = str(env)
+        self.agent_name = str(agent)
+    
     def before_episode(self, env, agent):
         return env.reset(), False, 0
+
+    def train_routine(self, env, agent, episodes=200, render=False, filename=None):
+        for episode in range(episodes):
+            state, done, steps = self.before_episode(env, agent)
+            tmp_loss = []
+            tmp_reward = []
+            actions_dbg = []
+            while not done:
+                if render and (episode+1)%10==0:
+                    env.render()
+                action = agent(state)
+                actions_dbg.append(action)
+                next, reward, done, _ = env.step(action)
+                self.memory.append(Experience(state, next, reward, action, done))
+                if len(self.memory) > self.batch:
+                    tmp_loss.append(self.experience_replay(episode, steps, agent))
+                tmp_reward.append(reward.data[0])                
+                state = next
+                steps += 1
+            if render and (episode+1)%10==0:
+                env.close()
+            self.after_episode(episode+1, steps, agent, tmp_loss, tmp_reward, filename)
+
+    def experience_replay(self, episode, step_count, agent):
+        experience, idx, weights = self.memory.sample(self.batch)
+        action_value, target_action_value = agent.get_train_signal(experience, self.gamma)
+        if isinstance(self.memory, PrioritizedMemory):
+            priorities = np.abs(target_action_value.data.reshape(-1) - action_value.data.reshape(-1) + 1e-5)**weights
+            self.memory.update_priorities(idx, priorities)
+        loss = agent.update(action_value, target_action_value)
+        return loss
 
     def after_episode(self, episode, steps, agent, loss, reward, filename=None):
         agent.episode_count += 1
@@ -42,43 +78,9 @@ class Trainer(object):
             if len(self.rewards) > 2:
                 if self.rewards[-1] >= max(self.rewards[:-2]):
                     agent.save(filename) 
-    
-    def before_train(self, env, agent):
-        self.env_name = str(env)
-        self.agent_name = str(agent)
-    
+
     def after_train(self):
         logger.info('[*] training finished with best score: {}'.format(max(self.rewards)))
-
-    def train_routine(self, env, agent, episodes=200, render=False, filename=None):
-        for episode in range(episodes):
-            state, done, steps = self.before_episode(env, agent)
-            tmp_loss = []
-            tmp_reward = []
-            while not done:
-                if render:
-                    env.render()
-                action = agent.policy(state)
-                next, reward, done, info = env.step(action)
-                self.memory.append(Experience(state, next, reward, action, done))
-                if len(self.memory) > self.batch:
-                    tmp_loss.append(self.experience_replay(episode, steps, agent))
-                tmp_reward.append(reward.data[0])                
-                state = next
-                steps += 1
-            self.after_episode(episode+1, steps, agent, tmp_loss, tmp_reward, filename)
-        self.after_train()
-
-    def experience_replay(self, episode, step_count, agent):
-        if isinstance(self.memory, PrioritizedMemory):
-            self.memory.beta = min(1.0, 0.4+step_count/100000)
-        batch, idx, weights = self.memory.sample(self.batch)
-        action_value, target_action_value = agent.get_train_signal(batch, self.gamma)
-        loss = agent.update(action_value, target_action_value)
-        if isinstance(self.memory, PrioritizedMemory):
-            priorities = weights * (action_value.data.reshape(-1) - target_action_value.data.reshape(-1))**2 + 1e-5
-            self.memory.update_priorities(idx, priorities)
-        return loss
     
     def plot(self, filename=None):
         plt.subplot(2, 1, 1)
