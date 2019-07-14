ae51a486ee741f52a3e5f549b3639e5292d9372c
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-07-14 18:25:15 -0500

    Update core.py

--- qualia2/rl/core.py
+++ qualia2/rl/core.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- 
 from ..core import *
 from ..autograd import Tensor
-from ..functions import huber_loss, amax, mse_loss
+from ..functions import huber_loss, amax
 import random
 import numpy
 import gym
@@ -10,8 +10,8 @@ from matplotlib import animation
 from logging import getLogger
 logger = getLogger('QualiaLogger').getChild('rl')
 
-class ValueAgent(object):
-    ''' ValueAgent \n
+class BaseAgent(object):
+    ''' BaseAgent \n
     Base class for value based agents. Some methods needs to be over ridden.
     Args:
         actions (list): list of actions
@@ -40,15 +40,8 @@ class ValueAgent(object):
     def __call__(self, observation, *args):
         return self.policy(observation, *args)
     
-    def policy(self, observation, *args, eps=None):
-        # returns action as numpy array
-        if eps is None:
-            eps = max(0.5*(1/(self.episode_count+1)), 0.05)
-        if random.random() < eps:
-            return numpy.random.choice(self.actions)
-        else:
-            self.model.eval()
-            return numpy.argmax(self.model(observation.reshape(1,-1), *args).asnumpy())
+    def policy(self, observation, *args):
+        raise NotImplementedError
         
     def save(self, filename):
         self.model.save(filename)
@@ -87,7 +80,7 @@ class ValueAgent(object):
         target_action_value = reward + gamma * next_state_action_value
         return state_action_value, target_action_value.detach()
 
-    def update(self, state_action_value, target_action_value, loss_func=mse_loss):
+    def update(self, state_action_value, target_action_value, loss_func=huber_loss):
         self.model.train()
         loss = loss_func(state_action_value, target_action_value)
         self.optim.zero_grad()
@@ -97,7 +90,49 @@ class ValueAgent(object):
     
     def update_target_model(self):
         self.target.load_state_dict(self.model.state_dict())
+
+class ValueAgent(BaseAgent):
+    def policy(self, observation, *args, eps=None):
+        # returns action as numpy array
+        if eps is None:
+            eps = max(0.5*(1/(self.episode_count+1)), 0.05)
+        if random.random() < eps:
+            return numpy.random.choice(self.actions)
+        else:
+            self.model.eval()
+            return numpy.argmax(self.model(observation.reshape(1,-1), *args).asnumpy())
     
+class PolicyAgent(BaseAgent):
+    def policy(self, observation, *args, eps=None):
+        # returns action as numpy array
+        if eps is None:
+            eps = max(0.5*(1/(self.episode_count+1)), 0.05)
+        if random.random() < eps:
+            return numpy.random.choice(self.actions)
+        else:
+            self.model.eval()
+            return numpy.random.choice(self.actions, p=self.model(observation.reshape(1,-1), *args).asnumpy())
+
+class ActorCriticAgent(BaseAgent):
+    def __init__(self, actions, actor, critic):
+        self.actions = actions
+        self.eps = 1
+        self.actor = actor
+        self.actor_target = actor
+        self.critic = critic
+        self.critic_target = critic
+        self.actor_optim = None
+        self.critic_optim = None
+        self.episode_count = 0
+
+    @classmethod
+    def init(cls, env, actor, critic):
+        actions = env.action_space.n
+        return cls(actions, actor, critic)
+
+    def policy(self, observation, *args, eps=None):
+        return self.actor(observation).asnumpy()
+
 class Env(object):
     ''' Env \n
     Wrapper class of gym.env for reinforcement learning.
