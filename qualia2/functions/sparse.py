144bdbe6290e6d2d6b3a012872153430918cad7f
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-07-14 17:14:48 -0500

    Update sparse.py

--- qualia2/functions/sparse.py
+++ qualia2/functions/sparse.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*- 
 from ..core import *
 from ..autograd import *
-from .linear_algebra import tensordot
 
 class Embedding(Function):
     @staticmethod
@@ -10,16 +9,24 @@ class Embedding(Function):
         Args:
             input (Tensor): Long Tensor containing indices into the embedding matrix
             weight (Tensor): The embedding matrix with number of rows equal to the maximum possible index + 1, and number of columns equal to the embedding size
+            vocab_size (int): 
         '''
-        return tensordot(Embedding.to_one_hot(input, vocab_size), weight)
+        if isinstance(input, Tensor):
+            result = Tensor(weight[input.data.astype('int64')].data)
+            result.set_creator(Embedding.prepare(result.shape, weight, idx=input.data))
+        elif isinstance(input, np.ndarray):
+            result = Tensor(weight[input.astype('int64')].data)
+            result.set_creator(Embedding.prepare(result.shape, weight, idx=input))
+        else:
+            raise ValueError
+        return result
 
-    @staticmethod
-    def to_one_hot(input, vocab_size):
-        corpus = input.data
-        dim = corpus.shape
-        one_hot = np.zeros((vocab_size,*dim))
-        for c in range(vocab_size):
-            one_hot[c][corpus==c] = 1
-        return Tensor(np.swapaxes(one_hot,0,-1), requires_grad=False)
+    def calc_grad(self, dx):
+        dw = np.zeros_like(self.var[0].data)
+        if gpu:
+            np.scatter_add(dw, self.kwargs['idx'].astype('int64'), dx)
+        else:
+            np.add.at(dw, self.kwargs['idx'].astype('int64'), dx)
+        return dw
 
 embedding = Embedding(None)
