7159d596e3257a7319b8e1487c50c8f7ad6c9327
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-08-02 06:05:43 -0500

    Update kuzushi_mnist.py

--- qualia2/data/kuzushi_mnist.py
+++ qualia2/data/kuzushi_mnist.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import os
 import gzip
 
-class KuzushiMNIST(DataLoader):
+class KuzushiMNIST(ImageLoader):
     '''KuzushiMNIST Dataset\n     
     Args:
         normalize (bool): If true, the intensity value of a specific pixel in a specific image will be rescaled from [0, 255] to [0, 1]. Default: True 
@@ -80,7 +80,7 @@ class KuzushiMNIST(DataLoader):
                 plt.imshow(to_cpu(img) if gpu else img, cmap='gray', interpolation='nearest') 
         plt.show()
 
-class Kuzushi49(DataLoader):
+class Kuzushi49(ImageLoader):
     '''Kuzushi49 Dataset\n     
     Args:
         normalize (bool): If true, the intensity value of a specific pixel in a specific image will be rescaled from [0, 255] to [0, 1]. Default: True 
