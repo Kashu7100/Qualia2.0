dd805c3c8c965001b1a5b6c9e385a7601e68f80a
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-08-05 19:05:09 -0500

    Update activation.py

--- qualia2/nn/modules/activation.py
+++ qualia2/nn/modules/activation.py
@@ -3,6 +3,9 @@ from .module import Module
 from ...functions import *
 
 class ReLU(Module):
+    def __repr__(self):
+        return '{}() at 0x{:0{}X}'.format(self.__class__.__name__, id(self), 16)
+
     def forward(self, x):
         if self.input_shape is None:
             self.input_shape = x.shape
@@ -11,6 +14,9 @@ class ReLU(Module):
         return relu(x)
 
 class LeakyReLU(Module):
+    def __repr__(self):
+        return '{}() at 0x{:0{}X}'.format(self.__class__.__name__, id(self), 16)
+
     def forward(self, x):
         if self.input_shape is None:
             self.input_shape = x.shape
@@ -22,6 +28,9 @@ class ELU(Module):
     def __init__(self, alpha=1.0):
         super().__init__()
         self.alpha = alpha
+    
+    def __repr__(self):
+        return '{}() at 0x{:0{}X}'.format(self.__class__.__name__, id(self), 16)
 
     def forward(self, x):
         if self.input_shape is None:
@@ -31,6 +40,9 @@ class ELU(Module):
         return elu(x, self.alpha)
 
 class Sigmoid(Module):
+    def __repr__(self):
+        return '{}() at 0x{:0{}X}'.format(self.__class__.__name__, id(self), 16)
+
     def forward(self, x):
         if self.input_shape is None:
             self.input_shape = x.shape
@@ -39,6 +51,9 @@ class Sigmoid(Module):
         return sigmoid(x)
 
 class Tanh(Module):
+    def __repr__(self):
+        return '{}() at 0x{:0{}X}'.format(self.__class__.__name__, id(self), 16)
+
     def forward(self, x):
         if self.input_shape is None:
             self.input_shape = x.shape
@@ -47,6 +62,9 @@ class Tanh(Module):
         return tanh(x)
 
 class SoftPuls(Module):
+    def __repr__(self):
+        return '{}() at 0x{:0{}X}'.format(self.__class__.__name__, id(self), 16)
+
     def forward(self, x):
         if self.input_shape is None:
             self.input_shape = x.shape
@@ -55,6 +73,9 @@ class SoftPuls(Module):
         return softplus(x)
 
 class SoftSign(Module):
+    def __repr__(self):
+        return '{}() at 0x{:0{}X}'.format(self.__class__.__name__, id(self), 16)
+
     def forward(self, x):
         if self.input_shape is None:
             self.input_shape = x.shape
@@ -63,14 +84,20 @@ class SoftSign(Module):
         return softsign(x)
 
 class SoftMax(Module):
+    def __repr__(self):
+        return '{}() at 0x{:0{}X}'.format(self.__class__.__name__, id(self), 16)
+
     def forward(self, x):
         if self.input_shape is None:
             self.input_shape = x.shape
         if self.output_shape is None:
             self.output_shape = x.shape
         return softmax(x)
-    
+
 class Flatten(Module):
+    def __repr__(self):
+        return '{}() at 0x{:0{}X}'.format(self.__class__.__name__, id(self), 16)
+        
     def forward(self, x):
         result = x.reshape(x.shape[0],-1)
         if self.input_shape is None:
