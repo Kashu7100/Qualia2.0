f0efef4084213da12419c5e49f8c989bcfcc5ea0
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-08-16 06:52:04 -0500

    Update util.py

--- qualia2/nlp/util.py
+++ qualia2/nlp/util.py
@@ -24,3 +24,28 @@ def most_similar(query, word2idx, wordvecs, n=5):
         if idx2word[int(idx[i])] == query:
             continue
         print('{}: {}'.format(idx2word[int(idx[i])], similarity[int(idx[i])]))
+
+def analogy(a, b, c, word2idx, wordvec, n=5):
+    ''' analogy\n
+    Predicts word relationship like a:b = c:?
+    Args:
+        a (str): input string
+        b (str): input string
+        c (str): input string
+        word2idx (dict): word to index map
+        wordvec (Embedding): ector representation of words
+        n (int): top n similar words to show
+    '''
+    assert a in word2idx
+    assert b in word2idx
+    assert c in word2idx
+
+    print('[*] analogy {}:{} = {}:?'.format(a,b,c))
+    idx2word = {v: k for k, v in word2idx.items()}
+    a_vec, b_vec, c_vec = wordvec(word2idx[a]), wordvec(word2idx[b]), wordvec(word2idx[c])
+    query_vec = b_vec - a_vec + c_vec
+    
+    similarity = np.array([float(cosine_similarity(wordvec(i), query_vec).data) for i in range(len(idx2word))])
+    idx = similarity.argsort()[::-1]
+    for i in range(n):
+        print('{}: {}'.format(idx2word[int(idx[i])], similarity[int(idx[i])]))
