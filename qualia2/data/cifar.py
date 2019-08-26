d3e0048eb3589b6dd29637563621bbc1668c3866
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-08-26 02:52:15 -0500

    Update cifar.py

--- qualia2/data/cifar.py
+++ qualia2/data/cifar.py
@@ -6,6 +6,7 @@ from .dataloader import ImageLoader
 import matplotlib.pyplot as plt
 import os
 import tarfile
+import pickle
 
 class CIFAR10(ImageLoader):
     '''CIFAR10 Dataset\n     
@@ -21,19 +22,15 @@ class CIFAR10(ImageLoader):
         path = os.path.dirname(os.path.abspath(__file__)) 
 
         print('[*] preparing data...')
-        if not os.path.exists(path + '/download/cifar10/'): 
-            print('    this might take few minutes.') 
-            os.makedirs(path + '/download/cifar10/') 
-            self.download(path+'/download/cifar10/')
-            self.extract(path+'/download/cifar10/')
+        self.download()
         self.train_data = np.empty((50000,3*32*32))
         self.train_label = np.empty((50000,10))
         for i in range(5):
-            self.train_data[i*10000:(i+1)*10000] = self._load_data(path + '/download/cifar10/cifar-10-batches-py/data_batch_{}'.format(i+1))
-            self.train_label[i*10000:(i+1)*10000] = CIFAR10.to_one_hot(self._load_label(path + '/download/cifar10/cifar-10-batches-py/data_batch_{}'.format(i+1)), 10)
+            self.train_data[i*10000:(i+1)*10000] = self._load_data(path + '/download/cifar10/cifar-10.tar.gz', i+1, 'train')
+            self.train_label[i*10000:(i+1)*10000] = CIFAR10.to_one_hot(self._load_label(path + '/download/cifar10/cifar-10.tar.gz', i+1, 'train'), 10)
 
-        self.test_data = self._load_data(path + '/download/cifar10/cifar-10-batches-py/test_batch')
-        self.test_label = CIFAR10.to_one_hot(self._load_label(path + '/download/cifar10/cifar-10-batches-py/test_batch'), 10)
+        self.test_data = self._load_data(path + '/download/cifar10/cifar-10.tar.gz', i+1, 'test')
+        self.test_label = CIFAR10.to_one_hot(self._load_label(path + '/download/cifar10/cifar-10.tar.gz', i+1, 'test'), 10)
         print('[*] done.')
 
         if normalize: 
@@ -42,7 +39,7 @@ class CIFAR10(ImageLoader):
         if not flatten:
             self.train_data = self.train_data.reshape(-1,3,32,32) 
             self.test_data = self.test_data.reshape(-1,3,32,32) 
-    
+
     @property
     def label_dict(self):
         return {
@@ -58,35 +55,30 @@ class CIFAR10(ImageLoader):
             9: 'truck'
         }
 
-    def download(self, path): 
-        import urllib.request 
+    def download(self): 
         url = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz' 
-        if not os.path.exists(path+'cifar-10.tar.gz'): 
-            urllib.request.urlretrieve(url, path+'cifar-10.tar.gz') 
-
-    def extract(self, path):
-        print('[*] extracting data...')
-        os.chdir(path)
-        file = tarfile.open('cifar-10.tar.gz')
-        file.extractall()
+        super().download(url, 'cifar-10.tar.gz')
     
-    def _unpickle(self, filename):
-        import pickle
-        with open(filename, 'rb') as file:
-            dict = pickle.load(file, encoding='bytes')
-        return dict
-    
-    def _load_data(self, filename):
-        if gpu:
-            import numpy
-            data = np.asarray(self._unpickle(filename)[b'data'])
-        else:
-            data = self._unpickle(filename)[b'data']
-        return data
+    def _load_data(self, filename, idx, data_type='train'):
+        assert data_type in ['train', 'test']
+        with tarfile.open(filename, 'r:gz') as file:
+            for item in file.getmembers():
+                if ('data_batch_{}'.format(idx) in item.name and data_type == 'train') or ('test_batch' in item.name and data_type == 'test'):
+                    data_dict = pickle.load(file.extractfile(item), encoding='bytes')
+                    if gpu:
+                        import numpy
+                        data = np.asarray(data_dict[b'data'])
+                    else:
+                        data = data_dict[b'data']
+                    return data
 
-    def _load_label(self, filename):
-        labels = np.array(self._unpickle(filename)[b'labels']) 
-        return labels
+    def _load_label(self, filename, idx, data_type='train'):
+        assert data_type in ['train', 'test']
+        with tarfile.open(filename, 'r:gz') as file:
+            for item in file.getmembers():
+                if ('data_batch_{}'.format(idx) in item.name and data_type == 'train') or ('test_batch' in item.name and data_type == 'test'):
+                    data_dict = pickle.load(file.extractfile(item), encoding='bytes')
+                    return np.array(data_dict[b'labels'])
 
     def show(self):
         for i in range(10):
@@ -113,16 +105,13 @@ class CIFAR100(ImageLoader):
         path = os.path.dirname(os.path.abspath(__file__)) 
 
         print('[*] preparing data...')
-        if not os.path.exists(path + '/download/cifar100/'): 
-            print('    this might take few minutes.') 
-            os.makedirs(path + '/download/cifar100/') 
-            self.download(path+'/download/cifar100/')
-            self.extract(path+'/download/cifar100/')
-        self.train_data = self._load_data(path + '/download/cifar100/cifar-100-python/train')
-        self.train_label = CIFAR100.to_one_hot(self._load_label(path + '/download/cifar100/cifar-100-python/train'), 100)
+        self.download()
+        
+        self.train_data = self._load_data(path + '/download/cifar100/cifar-100.tar.gz', 'train')
+        self.train_label = CIFAR100.to_one_hot(self._load_label(path + '/download/cifar100/cifar-100.tar.gz', 'train'), 100)
 
-        self.test_data = self._load_data(path + '/download/cifar100/cifar-100-python/test')
-        self.test_label = CIFAR100.to_one_hot(self._load_label(path + '/download/cifar100/cifar-100-python/test'), 100)
+        self.test_data = self._load_data(path + '/download/cifar100/cifar-100.tar.gz', 'test')
+        self.test_label = CIFAR100.to_one_hot(self._load_label(path + '/download/cifar100/cifar-100.tar.gz', 'test'), 100)
         print('[*] done.')
 
         if normalize: 
@@ -132,35 +121,30 @@ class CIFAR100(ImageLoader):
             self.train_data = self.train_data.reshape(-1,3,32,32) 
             self.test_data = self.test_data.reshape(-1,3,32,32) 
 
-    def download(self, path): 
-        import urllib.request 
+    def download(self): 
         url = 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz' 
-        if not os.path.exists(path+'cifar-100.tar.gz'): 
-            urllib.request.urlretrieve(url, path+'cifar-100.tar.gz') 
+        super().download(url, 'cifar-100.tar.gz')
+        
+    def _load_data(self, filename, data_type='train'):
+        assert data_type in ['train', 'test']
+        with tarfile.open(filename, 'r:gz') as file:
+            for item in file.getmembers():
+                if data_type in item.name:
+                    data_dict = pickle.load(file.extractfile(item), encoding='bytes')
+                    if gpu:
+                        import numpy
+                        data = np.asarray(data_dict[b'data'])
+                    else:
+                        data = data_dict[b'data']
+                    return data
 
-    def extract(self, path):
-        print('[*] extracting data...')
-        os.chdir(path)
-        file = tarfile.open('cifar-100.tar.gz')
-        file.extractall()
-    
-    def _unpickle(self, filename):
-        import pickle
-        with open(filename, 'rb') as file:
-            dict = pickle.load(file, encoding='bytes')
-        return dict
-    
-    def _load_data(self, filename):
-        if gpu:
-            import numpy
-            data = np.asarray(self._unpickle(filename)[b'data'])
-        else:
-            data = self._unpickle(filename)[b'data']
-        return data
-
-    def _load_label(self, filename):
-        labels = np.array(self._unpickle(filename)[b'fine_labels']) 
-        return labels
+    def _load_label(self, filename, data_type='train'):
+        assert data_type in ['train', 'test']
+        with tarfile.open(filename, 'r:gz') as file:
+            for item in file.getmembers():
+                if data_type in item.name:
+                    data_dict = pickle.load(file.extractfile(item), encoding='bytes')
+                    return np.array(data_dict[b'fine_labels'])
 
     def show(self):
         for i in range(100):
