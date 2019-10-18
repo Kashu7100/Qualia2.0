3f3919183ba2c127378ac5c9738400a4cb7c90a8
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-10-18 15:53:50 -0500

    Update mnist.py

--- qualia2/data/mnist.py
+++ qualia2/data/mnist.py
@@ -2,7 +2,7 @@
 from .. import to_cpu
 from ..core import *
 from ..autograd import Tensor
-from .dataloader import ImageLoader
+from .dataloader import *
 import matplotlib.pyplot as plt
 import os
 import gzip
