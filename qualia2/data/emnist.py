4a7c6a6b539abb016d535ed9d028bac6d9602a41
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-10-18 15:52:41 -0500

    Update emnist.py

--- qualia2/data/emnist.py
+++ qualia2/data/emnist.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- 
 from ..core import *
 from ..autograd import Tensor
-from .dataloader import ImageLoader
+from .dataloader import *
 import matplotlib.pyplot as plt
 import gzip
 import random
