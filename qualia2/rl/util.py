dd394f51a423d4cc5ca1ccf2385a37136fc55cdc
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-10-21 02:08:14 -0500

    Update util.py

--- qualia2/rl/util.py
+++ qualia2/rl/util.py
@@ -3,6 +3,8 @@ from ..core import *
 from .memory import Experience, PrioritizedMemory
 import matplotlib.pyplot as plt
 from logging import getLogger
+import os
+
 logger = getLogger('QualiaLogger').getChild('rl')
 
 class Trainer(object):
@@ -24,8 +26,23 @@ class Trainer(object):
     def __repr__(self):
         print('{}'.format(self.__class__.__name__))
 
-    def train(self, env, model, episodes=200, render=False, filename=None):
-        raise NotImplementedError  
+    @property
+    def defaults(self):
+        return {
+            'batch': 80,
+            'capacity': 1e5,
+            'gamma': 0.99
+        }
+    
+    def load_settings(self, defaults):
+        for key, value in defaults.items():
+            super().__setattr__(key, value)
+
+    def train(self, env, agent, episodes=200, render=False, filename=None):
+        self.before_train(env, agent)
+        self.train_routine(env, agent, episodes=episodes, render=render, filename=filename)
+        self.after_train()
+        return agent
     
     def before_train(self, env, agent):
         self.env_name = str(env)
@@ -35,26 +52,31 @@ class Trainer(object):
         return env.reset(), False, 0
 
     def train_routine(self, env, agent, episodes=200, render=False, filename=None):
-        for episode in range(episodes):
-            state, done, steps = self.before_episode(env, agent)
-            tmp_loss = []
-            tmp_reward = []
-            actions_dbg = []
-            while not done:
+        try:
+            for episode in range(episodes):
+                state, done, steps = self.before_episode(env, agent)
+                tmp_loss = []
+                tmp_reward = []
+                while not done:
+                    if render and (episode+1)%10==0:
+                        env.render()
+                    action = agent(state)
+                    next, reward, done, _ = env.step(action)
+                    self.memory.append(Experience(state, next, reward, action, done))
+                    if len(self.memory) > self.batch:
+                        tmp_loss.append(self.experience_replay(episode, steps, agent))
+                    tmp_reward.append(reward.data[0])                
+                    state = next
+                    steps += 1
                 if render and (episode+1)%10==0:
-                    env.render()
-                action = agent(state)
-                actions_dbg.append(action)
-                next, reward, done, _ = env.step(action)
-                self.memory.append(Experience(state, next, reward, action, done))
-                if len(self.memory) > self.batch:
-                    tmp_loss.append(self.experience_replay(episode, steps, agent))
-                tmp_reward.append(reward.data[0])                
-                state = next
-                steps += 1
-            if render and (episode+1)%10==0:
-                env.close()
-            self.after_episode(episode+1, steps, agent, tmp_loss, tmp_reward, filename)
+                    env.close()
+                self.after_episode(episode+1, steps, agent, tmp_loss, tmp_reward, filename)
+        except:
+            path = os.path.dirname(os.path.abspath(__file__))
+            if not os.path.exists(path + '/tmp/'):
+                os.makedirs(path + '/tmp/') 
+            agent.save(path + '/tmp/auto_save')
+            raise Exception('[*] Training aborted.')
 
     def experience_replay(self, episode, step_count, agent):
         experience, idx, weights = self.memory.sample(self.batch)
@@ -83,6 +105,7 @@ class Trainer(object):
         logger.info('[*] training finished with best score: {}'.format(max(self.rewards)))
     
     def plot(self, filename=None):
+        assert len(self.losses) > 0
         plt.subplot(2, 1, 1)
         plt.plot([i for i in range(len(self.losses))], self.losses)
         plt.title('training losses and rewards of {} agent in {} task'.format(self.agent_name, self.env_name))
